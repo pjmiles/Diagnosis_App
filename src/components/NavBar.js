5c26f1cad04cb267fb6c9a14b63997baac80a89d
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-10-05 15:45:25 +0100

    NavBar component created

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">Diagnosis</div>
+      <ul>
+        <li>Home</li>
+        <li>Diagnose</li>
+        <li>Appointment</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
