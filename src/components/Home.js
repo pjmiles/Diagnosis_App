a4387063caa25ccb0a8d35e180cfc2b6135efbbf
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-10-07 17:02:56 +0100

    installed react-dropdown-select, new token in axios file & map function for symptom state

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import "../styles/Home.css";
 import { symptoms_url } from "../api/axios";
-import { diagnosis_url } from "../api/axios";
+import { diagnosis_url, token } from "../api/axios";
+import { newSymptoms_url } from "../api/axios";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
@@ -22,44 +23,53 @@ const Home = () => {
       ...state,
       [e.target.name]: e.target.value,
     }));
+    console.log("Hello");
   };
 
   // function to get priliminary diagnosis from patient
-  const getDiagnosis = async (patientDetaiils) => {
-    let newUrl = "https://sandbox-healthservice.priaid.ch/diagnosis?symptoms=[30]&gender=male&year_of_birth=60&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InBvbm1pbGVhZGViaXNpQGdtYWlsLmNvbSIsInJvbGUiOiJVc2VyIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiMTEyNDIiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3ZlcnNpb24iOiIyMDAiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL2xpbWl0IjoiOTk5OTk5OTk5IiwiaHR0cDovL2V4YW1wbGUub3JnL2NsYWltcy9tZW1iZXJzaGlwIjoiUHJlbWl1bSIsImh0dHA6Ly9leGFtcGxlLm9yZy9jbGFpbXMvbGFuZ3VhZ2UiOiJlbi1nYiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvZXhwaXJhdGlvbiI6IjIwOTktMTItMzEiLCJodHRwOi8vZXhhbXBsZS5vcmcvY2xhaW1zL21lbWJlcnNoaXBzdGFydCI6IjIwMjItMTAtMDUiLCJpc3MiOiJodHRwczovL3NhbmRib3gtYXV0aHNlcnZpY2UucHJpYWlkLmNoIiwiYXVkIjoiaHR0cHM6Ly9oZWFsdGhzZXJ2aWNlLnByaWFpZC5jaCIsImV4cCI6MTY2NTA4ODgzNSwibmJmIjoxNjY1MDgxNjM1fQ.eGH_esopXJ2PJxz2O4nLtLYD1nnXuPRLdEA1xlOW_cg&format=json&language=en-gb"  
-      try {
-      // const result = await axios.get(
-      //   diagnosis_url +
-      //     `${patientDetaiils.symptoms}&${patientDetaiils.gender}&${patientDetaiils.year_of_birth}`
-      // );
-      const { data } = await axios.get(newUrl);
-      setDiagnosis(JSON.stringify(data));
-      console.log(data[0]);
-      setIsLoading(true);
+  const getSymptoms = async () => {
+    try {
+      const { data } = await axios.get(newSymptoms_url);
+      setSymptom(data);
     } catch (e) {
       console.log(e);
-      setIsLoading(false);
     }
   };
 
-  // const getSymptoms = async () => {
-  //   try {
-  //     const { data } = await axios.get(symptoms_url);
-  //     setSymptom(JSON.stringify(data));
-  //     console.log(data);
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
-  // };
+  // handle submit function
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const getDiagnosis = async () => {
+      let newUrl = "";
+      try {
+        // const result = await axios.get(
+        //   diagnosis_url +
+        //     `${patientDetaiils.symptoms}&${patientDetaiils.gender}&${patientDetaiils.year_of_birth}`
+        // )
+        // console.log(result)
+        const { data } = await axios.get(newUrl, {
+          headers: { Authorization: `${token}` },
+        });
+        setDiagnosis(JSON.stringify(data));
+        console.log(data[0]);
+        setIsLoading(true);
+      } catch (e) {
+        console.log(e);
+        setIsLoading(false);
+      }
+    };
+    getDiagnosis();
+  };
 
   useEffect(() => {
-    // getSymptoms();
-    getDiagnosis();
+    getSymptoms();
   }, []);
+
+  // if (!symptom.length) return ".....";
 
   return (
     <section className="home-section">
-      <form className="symptom-section">
+      <form className="symptom-section" onSubmit={handleSubmit}>
         <div className="heading">
           <h1>Checker</h1>
           <p>Please kindly provide your details in the fields below</p>
@@ -67,15 +77,18 @@ const Home = () => {
         <div className="form-control">
           <div className="input-control">
             <label htmlFor="symptoms">
-              <input
-                type="text"
-                name="symptoms"
-                placeholder="enter symptoms here"
+              <select
+                placeholder="Select symptom"
                 className="input-field"
-                value={patientDetaiils.symptoms}
-                onChange={handleInputChange}
                 required
-              />
+              >
+                <option>Select symptom</option>
+                {symptom.map((option) => (
+                  <option key={option.ID} value={option.ID}>
+                    {option.Name}
+                  </option>
+                ))}
+              </select>
             </label>
           </div>
           <div className="input-control">
@@ -120,14 +133,12 @@ const Home = () => {
         <div className="diagnosis result">
           {isLoading ? (
             <div>
-              {/* {diagnosis.map(item => {
-                return (
-                  <div key={item.ID}>
-                    <div>{item.Issue}</div>
-                  </div>
-                )
-              })} */}
               <div>{diagnosis}</div>
+              {/* {diagnosis.map((item) => (
+                <div key={item.Issue}>
+                  {item.Issue}
+                </div>
+              ))} */}
             </div>
           ) : (
             "Loading..."
